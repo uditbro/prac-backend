cc48c736033a373a05298d627d604343fc2bdc92
Dev poonia <devpoonia23@gmail.com>
2024-09-16 11:34:10 +0530

    utils codes

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,41 @@
+const asyncHandler = (requestHandler) =>{
+    (req, res, next) =>{
+        Promise.resolve(requestHandler(req, res, next)).catch((err)=>next(err))
+    }
+}
+
+export {asyncHandler}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const asyncHandler = () =>{}
+// const asyncHandler = (func) => () =>{}
+// const asyncHandler = (func) => async() =>{}
+
+    // const asyncHandler = (fn) => async(req, res, next) =>{                   try catch method
+    //     try{
+
+    //     }catch(error){
+    //         res.status(err.code || 500).json({
+    //             success: false,
+    //             message: err.message
+    //         })
+    //     }
+    // }
